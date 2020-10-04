c54426c83d13421060449eb4e80bea2bcf588820
László Görög <contact@nergal.xyz>
2020-10-04 19:40:46 +0200

    traffic: add more test cases

--- traffic/traffic_test.go
+++ traffic/traffic_test.go
@@ -3,12 +3,54 @@ package traffic
 import (
 	"bytes"
 	"context"
+	"io"
 	"math"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
+	"strings"
 	"testing"
 	"time"
 )
+
+func TestFlowControlMiddleware(t *testing.T) {
+	var buf bytes.Buffer
+	buf.Write(bytes.Repeat([]byte("0"), 1*MB))
+
+	handler := FlowControlMiddleware(500 * KB)
+	handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		io.Copy(w, &buf)
+	})
+
+	flowControl := handler(handlerFunc)
+	start := time.Now()
+	recorder := httptest.NewRecorder()
+	flowControl.ServeHTTP(recorder, httptest.NewRequest("GET", "/subdir/asdf", nil))
+
+	delay := time.Now().Sub(start)
+	wantDelay := 2 * time.Second
+	if math.Abs(wantDelay.Seconds()-delay.Seconds()) > 0.1 {
+		t.Errorf("ServeHTTP() delay = %v, wantDelay %v", delay, wantDelay)
+		return
+	}
+	got := recorder.Body.String()
+	want := buf.String()
+	if !strings.Contains(got, want) {
+		t.Fatalf("ServeHTTP() got = %v, want = %v", got, want)
+	}
+}
+
+func TestFlowControlResponseWriter(t *testing.T) {
+	got := FlowControlResponseWriter(nil, context.Background(), 1)
+	want := &FlowControlledResponseWriter{
+		ResponseWriter: nil,
+		ctx:            context.Background(),
+		bytesPerSec:    1,
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("FlowControlResponseWriter() = %v, want %v", got, want)
+	}
+}
 
 func TestFlowControlledResponseWriter_Write(t *testing.T) {
 	t.Parallel()
@@ -38,6 +80,22 @@ func TestFlowControlledResponseWriter_Write(t *testing.T) {
 			args:      args{p: bytes.Repeat([]byte("0"), 1*MB)},
 			wantDelay: 2 * time.Second,
 			want:      1 * MB,
+		},
+		{
+			name: "already closed context",
+			fields: fields{
+				ResponseWriter: httptest.NewRecorder(),
+				ctxProvider: func() context.Context {
+					ctx, cancel := context.WithCancel(context.Background())
+					cancel()
+					return ctx
+				},
+				bytesPerSec: 500 * KB,
+			},
+			args:      args{p: bytes.Repeat([]byte("0"), 1*MB)},
+			wantErr:   true,
+			wantDelay: 0,
+			want:      0,
 		},
 		{
 			name: "context cancellation",
