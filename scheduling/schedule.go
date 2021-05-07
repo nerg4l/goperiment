6c8e0694160ffd01d567da3cf1962d6fcd34d949
László Görög <contact@nergal.xyz>
2021-05-07 17:17:23 +0200

    scheduling: clarify use of absolute duration

--- scheduling/schedule.go
+++ scheduling/schedule.go
@@ -17,6 +17,10 @@ import (
 //
 // The first n execution can be skipped with an offset
 // greater then than the duration.
+//
+// Schedule operates on the time as an absolute duration since
+// the zero time. Thus, Schedule(_, Hour, _, _) may call f at
+// a non-zero minute, depending on the time's Location.
 func Schedule(ctx context.Context, p time.Duration, o time.Duration, f func(context.Context, time.Time)) {
 	// Position the first execution
 	first := time.Now().Truncate(p).Add(o)
